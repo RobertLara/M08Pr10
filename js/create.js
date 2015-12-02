4080a7d37675e8e174f1525132f0b794e9e36901
RobertLara <lara.robert9@gmail.com>
2015-12-02 20:02:51 +0100

    Update create.js

--- js/create.js
+++ js/create.js
@@ -1,11 +1,24 @@
 $('#btnPrivate').click(function () {
      $("i",this).toggleClass("fa-lock fa-unlock");
+     if($('#inputPrivate').val() == 'true'){
+         $('#inputPrivate').val() == 'false';
+     }else{
+         $('#inputPrivate').val() == 'true';
+     }
 }); 
 $('#btnWiki').click(function () {
      $("i",this).toggleClass("fa-comments fa-times");
+     if($('#inputWiki').val() == 'true'){
+         $('#inputWiki').val() == 'false';
+     }else{
+         $('#inputWiki').val() == 'true';
+     }
 }); 
 $('#btnDwn').click(function () {
      $("i",this).toggleClass("fa-download fa-times");
+     if($('#inputDown').val() == 'true'){
+         $('#inputDown').val() == 'false';
+     }else{
+         $('#inputDown').val() == 'true';
+     }
 }); 
-
-
