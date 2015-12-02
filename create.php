87ebdec87eb245cb013a2ca6ed02cc5c1b269d15
RobertLara <lara.robert9@gmail.com>
2015-12-02 20:04:36 +0100

    Update create.php

--- create.php
+++ create.php
@@ -1,7 +1,9 @@
 <?php
 require './api/Controller.php';
 $controller = new Controller();
-
+if (isset($_REQUEST['name'])) {
+    $reponse = $controller->createRepository($_REQUEST['name'], $_REQUEST['desc'], $_REQUEST['private'], $_REQUEST['wiki'], $_REQUEST['download'], $_REQUEST['license']);
+}
 include './tpl/header.php';
 ?>
 
@@ -10,14 +12,22 @@ include './tpl/header.php';
         <div class="modal-header">
             <h4 class="modal-title" id="myModalLabel">Creació d'un nou repositori</h4>
         </div>
-
+        <?php
+        if (isset($reponse)) {
+            if (isset($reponse->id)) {
+                echo '<div style="margin:20px;" class="alert alert-success"><strong>Repositori creat amb exit!</strong> Indicates a successful or positive action.</div>';
+            } else {
+                echo '<div style="margin:20px;" class="alert alert-danger"><strong>Error!</strong> El repositori no s\'ha creat correctamente.</div>';
+            }
+        }
+        ?>
         <div class="modal-body">
             <form id="registerForm" method="POST" >
                 <div class="form-group">
                     <div class="col-xs-6">
                         <label for="InputName">Nom</label>
                         <div class="input-group">
-                            <input type="text" class="form-control" name="first_name" placeholder="Nom" required>
+                            <input type="text" class="form-control" name="name" placeholder="Nom" required>
                             <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
                         </div>
                     </div>
@@ -29,8 +39,7 @@ include './tpl/header.php';
                             <button type="button" id="btnPrivate">
                                 <i  class="fa fa-2x fa-unlock"></i>
                             </button>
-
-                            <input type="hidden" class="form-control" name="private" required>
+                            <input id="inputPrivate" type="hidden" class="form-control" name="private" value="true">
                         </div>
                     </div> 
                     <div class="col-xs-3">
@@ -39,7 +48,7 @@ include './tpl/header.php';
                             <button type="button" id="btnWiki">
                                 <i  class="fa fa-2x fa-comments"></i>
                             </button>
-                            <input type="hidden" class="form-control" name="wiki" required>
+                            <input id="inputWiki" type="hidden" class="form-control" name="wiki" value="true">
                         </div>
                     </div> 
                 </div> 
@@ -48,20 +57,21 @@ include './tpl/header.php';
                     <div class="col-xs-12">
                         <label for="InputEmail">Descripció</label>
                         <div class="textarea-form-control">
-                            <textarea class="form-control" rows="3"></textarea>
+                            <textarea class="form-control" rows="3" name="desc">
+                                
+                            </textarea>
                         </div>
                         <br />
                     </div>
                 </div>
-
                 <div class="form-group">
                     <div class="col-xs-6">
                         <label for="InputStreetName">Llicencia</label>
-                        <select class="form-control">
-                            <option>Apache</option>
-                            <option>GPL</option>
-                            <option>MIT</option>
-                            <option>Mozilla</option>
+                        <select name="license" class="form-control">
+                            <option value="apache">Apache</option>
+                            <option value="glp">GPL</option>
+                            <option value="mit">MIT</option>
+                            <option value="mozilla">Mozilla</option>
                         </select>
                         <br /> 
                     </div> 
@@ -71,7 +81,7 @@ include './tpl/header.php';
                             <button type="button" id="btnDwn">
                                 <i  class="fa fa-2x fa-download"></i>
                             </button>
-                            <input type="hidden" class="form-control" name="download" required>
+                            <input id="inputDown" type="hidden" class="form-control" name="download" value="true">
                         </div>
                         <br /> 
                     </div> 
@@ -85,8 +95,6 @@ include './tpl/header.php';
         </div>
     </div>
 </div>
-
-
 
 <?php
 include './tpl/footer.php';
