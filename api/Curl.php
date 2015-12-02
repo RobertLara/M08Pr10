f545b6ea72304843a7521578813e847b7d84f976
RobertLara <lara.robert9@gmail.com>
2015-12-02 20:03:40 +0100

    Update Curl.php

--- api/Curl.php
+++ api/Curl.php
@@ -19,17 +19,15 @@ class Curl {
     }
 
     public function post($url, $data_string) {
-
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string))
-        );
-
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: php-curl",
+            "Authorization: token " . TOKEN,
+            "Content-Length: " . strlen($data_string)));
         $result = curl_exec($ch);
         return $result;
     }
@@ -66,9 +64,6 @@ class Curl {
         return $result;
     }
 
-    
-
 }
-
 
 ?>
