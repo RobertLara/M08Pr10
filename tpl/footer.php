1148f74396a6540f193ff93bb1820986f5f5d3ec
RobertLara <lara.robert9@gmail.com>
2015-12-09 20:34:48 +0100

    Update footer.php

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,8 +1,9 @@
 </div>
 <script src="js/jquery.min.js"></script>
-<script src="js/<?php echo substr(basename($_SERVER['PHP_SELF']), 0,-3)."js"; ?>"/> <!-- Importació del JavaScript de la pàgina actual-->   
-        <!-- Bootstrap Core JavaScript -->
+<script src="js/<?php echo substr(basename($_SERVER['PHP_SELF']), 0,-3)."js"; ?>"></script> <!-- Importació del JavaScript de la pàgina actual-->           
+<!-- Bootstrap Core JavaScript -->
         <script src="js/bootstrap.min.js"></script>
+        
     </body>
 
 </html>
